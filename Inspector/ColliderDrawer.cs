23137f87490afc8def1b6f59367c5b69df007d2c
Olsyx <darkatomish@gmail.com>
2019-05-07 08:07:21 +0200

    VariationChooser

---
+++ Inspector/ColliderDrawer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using HedraLibrary;
+using UnityEngine;
+
+namespace HedraLibrary.Inspector {
+    public class ColliderDrawer : MonoBehaviour {
+
+        public enum DrawOptions {
+            Own,
+            Children,
+            All
+        }
+
+        [SerializeField] DrawOptions draw;
+        [SerializeField] Color color = Color.blue;
+        private Collider2D[] colliders;
+
+        void Start() {
+            StoreColliders();
+        }
+
+        void StoreColliders() {
+            switch (draw) {
+                case DrawOptions.Own:
+                    colliders = GetComponents<Collider2D>();
+                    break;
+                case DrawOptions.Children:
+                    colliders = GetComponentsInChildren<Collider2D>();
+                    break;
+                case DrawOptions.All:
+                    colliders = FindObjectsOfType<Collider2D>();
+                    break;
+            }
+        }
+
+        void OnDrawGizmos() {
+            if (!Application.isPlaying) {
+                StoreColliders();
+            }
+
+            Gizmos.color = color;
+            for (int i = 0; i < colliders.Length; i++) {
+                Collider2D collider = colliders[i];
+                if (collider != null) {
+                    if (collider is BoxCollider2D) {
+                        Hedra.DrawWireCube(collider.bounds.center, ((BoxCollider2D)collider).size, collider.transform.rotation);
+                    } else {
+                        Hedra.DrawWireCube(collider.bounds.center, collider.bounds.size, collider.transform.rotation);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Inspector/VariationChooser.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using HedraLibrary;
+using UnityEngine;
+
+namespace HedraLibrary.Inspector {
+    public class VariationChooser : MonoBehaviour {
+
+        [SerializeField] protected int variation;
+    
+	    void Start () {
+            ChooseVariation();
+        }
+	
+        void ChooseVariation() {
+            if (variation < 0) {
+                variation = 0;
+            }
+
+            int childCount = this.transform.childCount;
+            if (variation >= childCount) {
+                variation = variation % childCount;
+            }
+                 
+            foreach (Transform child in this.transform) {
+                child.gameObject.SetActive(false);
+            }
+            this.transform.GetChild(variation).gameObject.SetActive(true);
+        }
+
+	    void OnDrawGizmos () {
+            if (Application.isPlaying) {
+                return;
+            }
+            ChooseVariation();
+        }
+    }
+
+}
